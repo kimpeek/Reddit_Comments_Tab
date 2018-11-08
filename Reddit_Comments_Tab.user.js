4732987ebb57c5aed638e5b091d92e478fa2b015
KimPeek <kimpeek@users.noreply.github.com>
2018-11-08 12:39:23 -0600

    Make script work with old.reddit.com

--- Reddit_Comments_Tab.user.js
+++ Reddit_Comments_Tab.user.js
@@ -2,17 +2,17 @@
 // @name         Reddit_Comments_Tab
 // @name:en      Reddit_Comments_Tab
 // @namespace    Reddit_Comments_Tab
-// @version      0.2
+// @version      0.3
 // @description  Add a comments tab to the main nav bar.
 // @author       https://github.com/kimpeek
-// @include      https://www.reddit.com*
+// @include      *reddit.com*
 // @grant        none
 // ==/UserScript==
 
 var current_url = document.URL;
 var no_button = new RegExp("comments|inbox|user|overview|prefs|about|message");
-var multireddit = new RegExp("^http(s)?:\/\/www.reddit.com\/me\/m\/[A-Za-z0-9_]+");
-var subreddit = new RegExp("^http(s)?:\/\/www.reddit.com\/r\/[A-Za-z0-9_]+");
+var multireddit = new RegExp("^http(s)?:\/\/(www|old).reddit.com\/me\/m\/[A-Za-z0-9_]+");
+var subreddit = new RegExp("^http(s)?:\/\/(www|old).reddit.com\/r\/[A-Za-z0-9_]+");
 
 function main(){
     // Check if on a page to ignore
@@ -22,14 +22,14 @@ function main(){
 
     // Check if a multireddit --> extract multi --> create button with multi
     if (multireddit.test(current_url)) {
-        multi = current_url.match(multireddit);
+        const multi = current_url.match(multireddit);
         create_button(multi[0].concat("/comments/"));
         return;
     }
 
     // Check if a subreddit --> extract sub --> create button with sub
     else if (subreddit.test(current_url)) {
-        sub = current_url.match(subreddit);
+        const sub = current_url.match(subreddit);
         create_button(sub[0].concat("/comments/"));
         return;
     }
